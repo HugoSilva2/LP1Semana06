ef703441295289f1103f99589c03655ce945d71e
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-04-11 22:02:10 +0100

    Prep Stage( colors and sphers )

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -6,6 +6,16 @@ namespace ColorSpheres
     {
         static void Main(string[] args)
         {
+            //colors
+            Color color1 = new Color(255, 50, 70);
+            Color color2 = new Color(255, 255, 255);
+            Color color3 = new Color(77, 84, 25, 55);
+
+            //Spheres
+            Sphere hampter = new Sphere(color1, 20);
+            Sphere jeff = new Sphere(color2, 40);
+            Sphere cube = new Sphere(color3, 10);
+
             
         }
     }
